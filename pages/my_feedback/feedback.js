ab284173395ff3376792c0583c265993e0940973
c903968008 <903968008@qq.com>
2020-03-06 21:14:54 +0800

    反馈信息

--- pages/my_feedback/feedback.js
+++ pages/my_feedback/feedback.js
@@ -1,11 +1,21 @@
 // pages/feedback/feedback.js
+const app = getApp()
+import api from '../../utils/wxRequest.js';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    TabCur: 0,
+    scrollLeft: 0,
+    tab: ['未查看', '未接受', '已接受'],
+    feedback: []
+  },
 
+  tabSelect(e) {
+    // console.log(e.currentTarget.dataset.id)
+    this.getFeedback(e.currentTarget.dataset.id)
   },
 
   toDetail(){
@@ -18,7 +28,34 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getFeedback(0)
+  },
 
+  //获取反馈列表
+  getFeedback(status){
+    var param = {
+      status: status
+    }
+    api.wxRequest.get('/feedback', param, res => {
+      if (res.code == 200) {
+        // console.log(res.data)
+        res.data.forEach(function (item, index) {
+          item.time = item.created_at.split(' ')[0]
+        })
+        this.setData({
+          feedback: res.data
+        })
+        // console.log(this.data.question)
+      } else if (res.code == 500) {
+        this.setData({
+          feedback: []
+        })
+      } else {
+        console.log(res.errMsg)
+      }
+    }, err => {
+      console.log(err)
+    })
   },
 
   /**
